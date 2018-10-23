47a3d302cf21d56442a77612b01372a0f0f83af7
Eldar Akhmetov <eldar.workx@gmail.com>
2018-10-23 23:43:46 +0700

    1.1.0

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from '..';
+import hello from '..';
 
-console.log("Welcome to the Brain Games!");
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+console.log('Welcome to the Brain Games!');
+hello(0);
